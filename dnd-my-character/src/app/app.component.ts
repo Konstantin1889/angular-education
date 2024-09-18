88dbb2e7db22e6060274e03a97a6646be9b42c0b
Константин Комарец <komarec.0407@gmail.com>
2024-09-18 23:07:06 +0500

    add input and button

--- dnd-my-character/src/app/app.component.ts
+++ dnd-my-character/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InputTextModule, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
